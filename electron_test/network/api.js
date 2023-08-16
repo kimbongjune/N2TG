5b83724021f000904a4ce11cbdfc13959c634b1d
김봉준 <nocdu112@naver.com>
2023-08-16 20:48:47 +0900

    티스토리 API를 요청하고 반환값을 화면에 전달하는 로직 작성
    티스토리 API를 요청하고 반환된 값을 화면에 전달하는 테스트 로직을 작성하였음

--- electron_test/network/api.js
+++ electron_test/network/api.js
@@ -8,6 +8,12 @@ window.electron.ipcRenderer.on('notion-response', (data) => {
     container.innerHTML = JSON.stringify(data, null, 2);
 });
 
+
+window.electron.ipcRenderer.on('tistory-response', (data) => {
+    const container = document.getElementById('results');
+    container.innerHTML = JSON.stringify(data, null, 2);
+});
+
 function fetchGithubData() {
     const githubToken = document.getElementById('githubToken').value;
     const username = document.getElementById('username').value;
@@ -24,4 +30,14 @@ function fetchNotionData() {
         return alert("빈문자는 안돼");
     }
     window.electron.ipcRenderer.send('notion-api-renderer', { notionApiKey, databaseId });
+}
+
+function fetchTistoryData() {
+    const tistoryAppIDInput = document.getElementById('tistoryAppIDInput').value;
+    const tistorySecretKeyInput = document.getElementById('tistorySecretKeyInput').value;
+    const tistoryBlogName = document.getElementById('tistoryBlogName').value;
+    if (!tistoryAppIDInput || !tistorySecretKeyInput || !tistoryBlogName)  {
+        return alert("빈문자는 안돼");
+    }
+    window.electron.ipcRenderer.send('tistory-api-renderer', { tistoryAppIDInput, tistorySecretKeyInput, tistoryBlogName });
 }
